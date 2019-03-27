e070816ac17cf8298ce594b26778d9dd880d119c
egor5q <31898567+egor5q@users.noreply.github.com>
2019-03-28 00:18:47 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
+import os
+import telebot
+import time
+import random
+import threading
+from emoji import emojize
+from telebot import types
+from pymongo import MongoClient
+
+
+
+class Game:
+    
+    def __init__(self):
+        self.players={}
+        self.turn=1
+        self.currentplayer=None
+
 
