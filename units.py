50a40032fbb0749396c5f0adaeb81e6268fd290b
egor5q <31898567+egor5q@users.noreply.github.com>
2019-03-28 00:21:53 +0300

    Update units.py

--- units.py
+++ units.py
@@ -6,24 +6,26 @@ classes=['warrior', 'bower', 'mage', 'assasin', 'basic']
 
 
 class Unit:
-    self.hp=200
-    self.dmg=[16, 28]
-    self.luck=50
-    self.body={
-        'righthand':None,
-        'lefthand':None,
-        'head':None
-        
-    }
-    self.mainhand='righthand'
-    self.inentory=[]
-    self.class='basic'
-    self.mana=300
-    self.skills=[]
-    self.speedregen=400
-    self.speed=0
-    self.weight=800     # В ход у юнита регенится (self.speedregen) скорости. На ход юнит трати (self.weight) скорости. У кого на данный момент 
-    self.agility=50     # больше скорости - тот ходит. Если одинаково - рандом.
+    
+    def __init__(self):
+        self.hp=200
+        self.dmg=[16, 28]
+        self.luck=50
+        self.body={
+            'righthand':None,
+            'lefthand':None,
+            'head':None
+            
+        }
+        self.mainhand='righthand'
+        self.inentory=[]
+        self.class='basic'
+        self.mana=300
+        self.skills=[]
+        self.speedregen=400
+        self.speed=self.speedregen
+        self.weight=400     # В ход у юнита регенится (self.speedregen) скорости. На ход юнит трати (self.weight) скорости. 
+        self.agility=50     # У кого на данный момент больше скорости - тот ходит. Если одинаково - рандом.
     
     
     def recievedmg(self, unit, weapon):
@@ -49,10 +51,12 @@ class Unit:
     
     
 class Warrior(Unit):
-    super().__init__()
-    self.hp=int(self.hp*1.35)
-    self.dmg=self.changedmg(self.dmg, 0.75)
-    self.class='warrior'
-    self.agility=int(self.agility*0.7)
+    
+    def __init__(self):
+        super().__init__()
+        self.hp=int(self.hp*1.35)
+        self.dmg=self.changedmg(self.dmg, 0.75)
+        self.class='warrior'
+        self.agility=int(self.agility*0.7)
     
     
